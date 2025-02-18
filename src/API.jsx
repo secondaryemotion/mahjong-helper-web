e2f518915e1bdf7cc27e8fde17de34b926b44009
secondaryemotion <secondaryemotion@gmail.com>
2025-02-19 01:32:47 +0300

    ip edit

--- src/API.jsx
+++ src/API.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default async function requestData(hand) {
     try {
-        const response = await axios.get("https://178.64.127.20:8080/analyseHand?hand=" + hand).then((response) => response.data)
+        const response = await axios.get("https://localhost:8080/analyseHand?hand=" + hand).then((response) => response.data)
         let handVariationTiles = response.handVariationTiles
         console.log(handVariationTiles)
         let handVariationWaitsTiles = response.handVariationWaitsTiles
